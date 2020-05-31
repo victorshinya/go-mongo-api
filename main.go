3540a7e22142c55fad3f67a37687ab12ea96ee0d
Victor Shinya <contato@victorshinya.com.br>
2020-05-30 22:12:26 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) { w.Write([]byte("v0.0.1 is up and running")) })
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
